5b104e3bb890b6fbd20bf18c0f3abdd005f87df3
asd0999 <obelixotic@gmail.com>
2019-04-04 12:59:55 -0400

    static page + node curl

---
+++ week-1/nodecurl.js
@@ -0,0 +1,12 @@
+let url = process.argv[2];
+
+const axios = require('axios');
+
+axios.get(url)
+  .then(response => {
+    console.log(url);
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
