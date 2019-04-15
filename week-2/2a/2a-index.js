144145fc73bfc4308257d1d90f13458fe091e135
asd0999 <obelixotic@gmail.com>
2019-04-15 15:37:37 -0400

    W2 HW

--- week-2/2a/2a-index.js
+++ week-2/2a/2a-index.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res) {
   res.send("Hey there!");
 });
 
+app.get('/dwd', function(req, res) {
+  res.send("our second route!");
+});
+
 app.listen(3000, function() {
   console.log('Our first web server! Listeing on port 3000');
 });
