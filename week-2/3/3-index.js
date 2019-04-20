ae369b614a4cf3d7af9a4fcc3f652edbb6103d2f
asd0999 <obelixotic@gmail.com>
2019-04-20 18:39:18 -0400

    w2 hw

--- week-2/3/3-index.js
+++ week-2/3/3-index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var mustacheExpress = require('mustache-express');
+var bodyParser = require('body-parser')
 var app = express();
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}));
 
 var insults = ['stupid', 'wanna-be', 'sucks assss', 'sucks so hard', ''];
 
@@ -17,10 +23,10 @@ app.post('/bar', function(req, res) {
   let rand_num = Math.floor(Math.random() * Math.floor(insults.length - 1))
   let rand_insult = insults[rand_num];
   if (rand_num < 2) {
-    res.write("<h1>What??? " + req.query.name + ` is such a ${rand_insult} band` + "</h1>");
+    res.write("<h1>What??? " + req.body.name + ` is such a ${rand_insult} band` + "</h1>");
     res.write('</body></html>');
   } else {
-    res.write("<h1>Whats wrong with you?! " + req.query.name + ` ${rand_insult}!` + "</h1>");
+    res.write("<h1>Whats wrong with you?! " + req.body.name + ` ${rand_insult}!` + "</h1>");
     res.write('</body></html>');
   }
 });
