3062133559c0cf10a6eb68f2d85784724abe56ed
asd0999 <obelixotic@gmail.com>
2019-05-02 19:08:13 -0400

    test fetch post request

---
+++ week-4/app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+app.use(express.static('./public'));
+
+var bodyParser = require("body-parser");
+const url = require('url');
+const path = require('path');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+const PORT = process.env.PORT || 8000;
+// var {
+//   Client
+// } = require('pg');
+// var client;
+//
+// client = new Client({
+//   database: 'inclass3'
+// });
+//
+// client.connect();
+
+app.post('/log', function(req, res) {
+  // console.log(JSON.stringify(req.body, null, null));
+  // var data = req.body;
+  console.log(req.body);
+
+  // client.query('INSERT INTO neural (nodes, mutation, generation) VALUES ($1, $2, $3)', [req.body.nodes, req.body.mutation, req.body.generation], function(error, results) {
+  //   if (error) {
+  //     console.log(error);
+  //   }
+  //   console.log('success!');
+  // });
+});
+
+app.listen(PORT, function() {
+  console.log('Server up! Listening on port: ' + PORT);
+});
