f05145bdca44aa5447dc3b3e51fe45082caab053
asd0999 <obelixotic@gmail.com>
2019-05-04 10:04:16 -0400

    getting back to client from server/db

--- week-4/app.js
+++ week-4/app.js
@@ -12,12 +12,34 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8000;
 
+var {
+  Client
+} = require('pg');
+var client;
+
+client = new Client({
+  database: 'inclass3'
+});
+
+client.connect();
+
 app.post('/log', function(req, res) {
   console.log(req.body);
-  let response = 47;
-
-  res.send({
-    'heyo': 99
+  client.query('INSERT INTO neural (nodes, mutation, generation) VALUES ($1, $2, $3)', [req.body.nodes, req.body.mutation, req.body.generation], function(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    client.query('SELECT * FROM neural', function(err, res2) {
+      if (err) {
+        console.log(error);
+      }
+      let message = [];
+      for (let row of res2.rows) {
+        message.push(row);
+      }
+      // console.log(message);
+      // res.send(message);
+    });
   });
 });
 
--- week-4/public/client.js
+++ week-4/public/client.js
@@ -5,18 +5,18 @@ postData("/log", {
     mutation: x,
     generation: 100
   })
-  .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+  // .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
   .catch(error => console.error(error));
 
 function postData(url, data) {
-  // Default options are marked with *
+  console.log(url, data);
   return fetch(url, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      method: "POST",
       headers: {
-        "Content-Type": "application/json",
-        // "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
+      body: JSON.stringify(data)
     })
-    .then(response => console.log(response.json())); // parses JSON response into native Javascript objects
+    .then(response => console.log(response.json()));
+
 }
